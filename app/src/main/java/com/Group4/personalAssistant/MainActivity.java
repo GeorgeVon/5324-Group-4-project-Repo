eac23fb61962c12b76b755824428ea4b61f55cd0
George Vondra <gxv8682@mavs.uta.edu>
2026-04-08 19:17:08 -0500

    basic ui

---
+++ app/src/main/java/com/Group4/personalAssistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.Group4.personalAssistant;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
